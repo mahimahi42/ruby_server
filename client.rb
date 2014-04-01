4ef68cf8c6497bc67bfb05681ad9ed5830511b51
Bryce Davis <bad1128@gmail.com>
2014-04-01 11:44:29 -0400

    Work on POST requests

--- client.rb
+++ client.rb
@@ -1,11 +1,34 @@
 require 'socket'
+require 'json'
 
 hostname = "localhost"
 port = 2000
 
 s = TCPSocket.open(hostname, port)
 
-while line = s.gets
-    puts line.chop
+command = ""
+while command != "q"
+    print "What kind of request? (q to quit) "
+    command = gets.chomp
+    case command
+        when "q" then break
+        when "POST" then
+            print "Name: "
+            name = gets.chomp
+            print "Email: "
+            email = gets.chomp
+            user = {}
+            user["name"] = name
+            user["email"] = email
+            viking = {}
+            viking["viking"] = user
+            data = viking.to_json
+            header = "POST /thanks.html HTTP/1.0\r\n\r\n"
+            extra = "Content-Type: text/json\nContent-Length: #{data.length}\n"
+            resp = "#{header}#{extra}#{data}"
+            s.print resp
+            puts s.read.split("\r\n\r\n", 2)[1]
+    end
 end
+
 s.close
--- server.rb
+++ server.rb
@@ -1,10 +1,13 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.open(2000)
 loop {
     client = server.accept
     response = client.gets
-    #puts(response)
+    puts "\n\n\n===========================\n"
+    puts response
+    puts "\n\n\n===========================\n"
     if response.chomp =~ /GET \/index.html HTTP\/1.0/
         file = response.chomp.split(" ")[1][1..-1]
         if File.exists?(file)
@@ -14,11 +17,32 @@ loop {
             resp += "Content-Length: #{cont.length}\n"
             resp += cont
             client.puts(resp)
-            #puts(resp)
         else
             resp = "HTTP/1.0 404 Not Found\r\n\r\n"
             client.puts(resp)
         end
+    elsif response.chomp.include? "POST /thanks.html HTTP/1.0"
+        puts "START"
+        headers = ""
+        puts client.gets
+        #headers = client.gets.split("\n")
+        puts headers
+        params = JSON.parse(headers[3])
+        puts params
+        File.open("thanks.html", "w") do |line|
+            if line.eql? "<%= yield %>"
+                viks = ""
+                params.each do |viking|
+                    name = "<li>Name: #{viking["name"]}</li>"
+                    email = "<li>Email: #{viking["email"]}</li>"
+                    viks += "#{name}#{email}<br />"
+                end
+                line = viks
+            end
+        end 
+        cont = File.open("thanks.html", "r").read
+        client.puts(cont)
+        puts "DONE"
     else
         resp = "HTTP/1.0 404 Not Found\r\n\r\n"
         client.puts(resp)
