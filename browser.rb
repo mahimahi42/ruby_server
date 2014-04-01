a9a11d72309fd5ae80b12f53dc29a79914926f51
Bryce Davis <bad1128@gmail.com>
2014-04-01 10:37:57 -0400

    Server now serves index.html to mini browser

--- browser.rb
+++ browser.rb
@@ -1,8 +1,8 @@
 require "socket"
 
-host = "www.tutorialspoint.com"
-port = 80
-path = "/index.htm"
+host = "localhost"
+port = 2000
+path = "/index.html"
 
 request = "GET #{path} HTTP/1.0\r\n\r\n"
 
@@ -10,4 +10,4 @@ socket = TCPSocket.open(host, port)
 socket.print(request)
 response = socket.read
 headers, body = response.split("\r\n\r\n", 2)
-print body
+puts body
--- server.rb
+++ server.rb
@@ -3,7 +3,25 @@ require 'socket'
 server = TCPServer.open(2000)
 loop {
     client = server.accept
-    client.puts(Time.now.ctime)
-    client.puts "Closing the connection. Bye!"
+    response = client.gets
+    #puts(response)
+    if response.chomp =~ /GET \/index.html HTTP\/1.0/
+        file = response.chomp.split(" ")[1][1..-1]
+        if File.exists?(file)
+            resp = "HTTP/1.0 200 OK\r\n\r\n"
+            cont = File.open(file, "r") {|f| f.read}
+            resp += "\nContent-Type: text/html\n"
+            resp += "Content-Length: #{cont.length}\n"
+            resp += cont
+            client.puts(resp)
+            #puts(resp)
+        else
+            resp = "HTTP/1.0 404 Not Found\r\n\r\n"
+            client.puts(resp)
+        end
+    else
+        resp = "HTTP/1.0 404 Not Found\r\n\r\n"
+        client.puts(resp)
+    end
     client.close
 }
